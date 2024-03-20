971a72ca6c8004c63f44216377b79fa2616ed88d
Sridhar <sridhar.fullstack.dev.india@gmail.com>
2024-03-20 17:13:06 +0530

    commit

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,10 +1,22 @@
 import { motion } from "framer-motion";
 import gsap from "gsap";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function Preloader() {
   const preloaderRef = useRef(null);
   const preloaderObserver = useRef(null);
+  const circleLogoLightRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(circleLogoLightRef.current, {
+      rotation: 360,
+      ease: "power1.inOut",
+      duration: 10,
+      repeat: -1,
+    });
+  }, []);
+
   const [loadingPercentage, setLoadingPercentage] = useState(0);
 
   useEffect(() => {
@@ -41,21 +53,21 @@ export default function Preloader() {
   }, []);
 
   return (
-    <section className="fixed top-0 left-0 h-screen w-screen bg-red-600 text-white hero-font overflow-hidden z-[99999]">
+    <section className="fixed top-0 left-0 h-screen w-screen bg-red-600 text-white preloader-font overflow-hidden z-[99999]">
       <motion.div
         initial={{ opacity: 0, x: "-100px" }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ ease: "easeInOut", duration: 2, delay: 0.1 }}
-        className="text-6xl"
+        className="mt-16 text-7xl"
       >
-        Welcome To Tuticorin's Best Car Fitting Company
+        Welcome To Tuticorin&apos;s Best Car Fitting Company
       </motion.div>
 
       <motion.div
         initial={{ opacity: 0, x: "-100px" }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ ease: "easeInOut", duration: 2, delay: 1 }}
-        className="text-8xl"
+        className="mt-16 text-2xl"
       >
         Maria Cars & Extra Fitting
       </motion.div>
@@ -64,12 +76,37 @@ export default function Preloader() {
         initial={{ width: "0%" }}
         animate={{ width: `${loadingPercentage}%` }}
         transition={{ ease: "easeInOut", duration: 2 }}
-        className="bg-white h-1 mt-8 rounded-xl"
+        className="bg-white h-2"
       ></motion.div>
 
       <div className="w-60 overflow-hidden">
-        <div ref={preloaderRef} className="text-xl">0 %</div>
+        <div ref={preloaderRef} className="text-5xl hero-font tracking-wide">
+          0 %
+        </div>
         <div className="text-sm py-4"></div>
+      </div>
+
+      <div className="h-36 w-36 bg-red-600 rounded-full fixed bottom-32 right-10">
+        <div className="relative bg-red-600 h-36 w-36 rounded-full">
+          <Image
+            ref={circleLogoLightRef}
+            src={"/assets/circle-logo.png"}
+            alt="Maria Cars & Fitting"
+            height={1000}
+            width={1000}
+            className="w-36 rounded-full pointer-events-none"
+          />
+
+          <div className="text-4xl absolute top-6 left-9 hero-font">
+            <Image
+              src={"/assets/M.png"}
+              alt=""
+              height={500}
+              width={500}
+              className="w-20"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
