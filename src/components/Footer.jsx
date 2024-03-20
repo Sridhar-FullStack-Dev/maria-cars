9dd90a7358836ed0f786767374d7645b1e4b42d9
Sridhar <sridhar.fullstack.dev.india@gmail.com>
2024-03-20 22:46:05 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,178 @@
+import gsap from "gsap";
+import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { Poppins } from "next/font/google";
+import { useLenis } from "@studio-freight/react-lenis";
+
+// Icons
+import { IoCallOutline } from "react-icons/io5";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700", "600", "500", "400", "300", "200", "100"],
+});
 
 export default function Footer() {
+  const footerParallaxRef = useRef(null);
+  const year = new Date().getFullYear();
+
+  useEffect(() => {
+    gsap.to(footerParallaxRef.current, {
+      y: 200,
+      ease: "power1.inOut",
+      delay: 0.3,
+      scrollTrigger: {
+        trigger: footerParallaxRef.current,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  }, []);
+
+  // Lenis
+  const lenis = useLenis();
+
   return (
     <motion.footer
       initial={{ background: "#FFF" }}
       whileInView={{ background: "#DC2C2C" }}
       transition={{ ease: "linear", duration: 2 }}
-      className="px-8 py-16 rounded-t-3xl text-white overflow-hidden"
+      className="footer-font px-8 pt-16 rounded-t-3xl text-white relative overflow-hidden"
     >
-      <div className="footer-font text-6xl">
-        <h1 className="mt-8">Upgrade Your Ride:</h1>
-        <h1 className="mt-8">Start Fitting your cars</h1>
-        <h1 className="mt-8">& Luxury Fittings !</h1>
+      <div className="flex justify-between items-center pr-8">
+        <div className="text-5xl relative z-20">
+          <h1 className="mt-8">Upgrade Your Ride</h1>
+          <h1 className="mt-8">Start Fitting your cars !</h1>
+        </div>
+
+        <div className="relative h-36 w-36 rounded-full">
+          <div className="relative bg-red-600 h-36 w-36 rounded-full">
+            <Image
+              src={"/assets/circle-logo.png"}
+              alt="Maria Cars & Fitting"
+              height={1000}
+              width={1000}
+              className="w-36 rounded-full pointer-events-none"
+            />
+
+            <div className="text-4xl absolute top-6 left-9 hero-font">
+              <Image
+                src={"/assets/M.png"}
+                alt=""
+                height={500}
+                width={500}
+                className="w-20 pointer-events-none"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        ref={footerParallaxRef}
+        className="footer-font text-8xl absolute top-0 left-0 opacity-5 z-10"
+      >
+        <h1 className="mt-8">Upgrade Your Ride</h1>
+        <h1 className="mt-8">Start Fitting your cars !</h1>
+      </div>
+
+      <div className="w-full flex justify-between items-start text-[14px] mt-32 relative z-20">
+        <div>
+          <h1>Address</h1>
+          <div className={`${poppins.className} font-light text-[12px] mt-2`}>
+            <p>113/3 Polpettai, Near New Bus Stand,</p>
+            <p>Opposite to perison plaza,</p>
+            <p>Thoothukudi-2, IND.</p>
+          </div>
+        </div>
+
+        <div>
+          <h1>Links</h1>
+          <div
+            className={`${poppins.className} font-light text-[12px] flex flex-col justify-center gap-2 mt-2`}
+          >
+            {footerLink.map((link, linkKey) => (
+              <Link
+                key={linkKey}
+                href={link.href}
+                onClick={() => lenis.scrollTo(`${link.href}`, { lerp: 0.02 })}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h1>Terms & Conditions</h1>
+          <div className={`${poppins.className} font-light text-[12px] mt-2`}>
+            <p>Exciting offers ends soon!</p>
+            <p>Data Privacy Check.</p>
+            <p>This Website Contains Cookie.</p>
+          </div>
+        </div>
+
+        <div>
+          <h1>Contacts</h1>
+          <div className={`${poppins.className} font-light text-[12px] mt-2`}>
+            <Link
+              href={"tel:+919443041599"}
+              className="flex items-center justify-center gap-2"
+            >
+              {" "}
+              <IoCallOutline /> +91 94430 41599
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center text-[12px] mt-8 pt-10 pb-5">
+        <div
+          className={`${poppins.className} flex items-center justify-center`}
+        >
+          Design by{" "}
+          <Link
+            href={"https://sridhar-portfolio-ashen.vercel.app/"}
+            target="_blank"
+            className="footer-font ml-2"
+          >
+            Sridhar E.
+          </Link>
+        </div>
+
+        <div
+          className={`${poppins.className} flex items-center justify-center`}
+        >
+          copyright <div className="footer-font mx-2">Magnetize</div>
+          {year}
+        </div>
       </div>
     </motion.footer>
   );
 }
+
+const footerLink = [
+  {
+    href: "#home",
+    name: "Home",
+  },
+  {
+    href: "#about",
+    name: "About",
+  },
+  {
+    href: "#services",
+    name: "Services",
+  },
+  {
+    href: "#featured-products",
+    name: "Featured Products",
+  },
+  {
+    href: "#contacts",
+    name: "Contacts",
+  },
+];
